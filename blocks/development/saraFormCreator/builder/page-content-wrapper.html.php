805f0b7cfb67023c1be64df0228fb91b442f4e0b
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2015-07-21 20:13:11 -0500

    Constructor de formularios SARA Form Creator v 0.0.0.1

---
+++ blocks/development/saraFormCreator/builder/page-content-wrapper.html.php
@@ -0,0 +1,132 @@
+<!-- Page Content -->
+<div id="page-content-wrapper">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-6">
+				<div class="panel-group" id="accordion">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title">
+								<a data-toggle="collapse" data-parent="#accordion"
+									href="#collapseOne"><span
+									class="glyphicon glyphicon-folder-open"> </span>Contenedores</a>
+							</h4>
+						</div>
+						<div id="collapseOne" class="panel-collapse collapse in">
+							<div class="panel-body">
+								<div class='row-fluid'>
+									<ol class='nested_with_no_drop vertical'>
+										<li class='highlight'><i class='icon-move'></i> formulario <i
+											class='icon-config' value='formulario'></i> <i
+											class='icon-remove' removable='false'></i>
+											<ol>
+											</ol></li>
+										<li class='highlight'><i class='icon-move'></i> division <i
+											class='icon-config' value='division'></i> <i
+											class='icon-remove' removable='false'></i>
+											<ol>
+											</ol></li>
+									</ol>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title">
+								<a data-toggle="collapse" data-parent="#accordion"
+									href="#collapseTwo"><span class="glyphicon glyphicon-th"> </span>Componentes
+									básicos</a>
+							</h4>
+						</div>
+						<div id="collapseTwo" class="panel-collapse collapse">
+							<div class="panel-body">
+								<div class='row-fluid'>
+									<ol class='nested_with_no_drop vertical'>
+										<li><i class='icon-move'></i> campoBoton <i
+											class='icon-config' value='campoBoton'></i> <i
+											class='icon-remove' removable='false'></i></li>
+										<li><i class='icon-move'></i> campoCuadroTexto <i
+											class='icon-config' value='campoCuadroTexto'></i> <i
+											class='icon-remove' removable='false'></i></li>
+										<li><i class='icon-move'></i> campoMensaje <i
+											class='icon-config' value='campoMensaje'></i> <i
+											class='icon-remove' removable='false'></i></li>
+									</ol>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title">
+								<a data-toggle="collapse" data-parent="#accordion"
+									href="#collapseThree"><span class="glyphicon glyphicon-list"> </span>Otros
+									componentes</a>
+							</h4>
+						</div>
+						<div id="collapseThree" class="panel-collapse collapse">
+							<div class="panel-body">
+								<div class='row-fluid'>
+									<ol class='nested_with_no_drop vertical'>
+										<li><i class='icon-move'></i> campoBotonRadial <i
+											class='icon-config' value='campoBotonRadial'></i> <i
+											class='icon-remove' removable='false'></i></li>
+										<li><i class='icon-move'></i> campoCuadroLista <i
+											class='icon-config' value='campoCuadroLista'></i> <i
+											class='icon-remove' removable='false'></i></li>
+										<li><i class='icon-move'></i> campoCuadroSeleccion <i
+											class='icon-config' value='campoCuadroSeleccion'></i> <i
+											class='icon-remove' removable='false'></i></li>
+										<li><i class='icon-move'></i> campoFecha <i
+											class='icon-config' value='campoFecha'></i> <i
+											class='icon-remove' removable='false'></i></li>
+										<li><i class='icon-move'></i> campoImagen <i
+											class='icon-config' value='campoImagen'></i> <i
+											class='icon-remove' removable='false'></i></li>
+										<li><i class='icon-move'></i> campoTextArea <i
+											class='icon-config' value='campoTextArea'></i> <i
+											class='icon-remove' removable='false'></i></li>
+										<li><i class='icon-move'></i> cuadro_lista <i
+											class='icon-config' value='cuadro_lista'></i> <i
+											class='icon-remove' removable='false'></i></li>
+										<li><i class='icon-move'></i> cuadro_texto <i
+											class='icon-config' value='cuadro_texto'></i> <i
+											class='icon-remove' removable='false'></i></li>
+										<li><i class='icon-move'></i> enlace <i class='icon-config'
+											value='enlace'></i> <i class='icon-remove' removable='false'></i>
+										</li>
+										<li><i class='icon-move'></i> enlaceWiki <i
+											class='icon-config' value='enlaceWiki'></i> <i
+											class='icon-remove' removable='false'></i></li>
+										<li><i class='icon-move'></i> listaNoOrdenada <i
+											class='icon-config' value='listaNoOrdenada'></i> <i
+											class='icon-remove' removable='false'></i></li>
+										<li><i class='icon-move'></i> marcoAgrupacion <i
+											class='icon-config' value='marcoAgrupacion'></i> <i
+											class='icon-remove' removable='false'></i></li>
+										<li><i class='icon-move'></i> recaptcha <i class='icon-config'
+											value='recaptcha'></i> <i class='icon-remove'
+											removable='false'></i></li>
+									</ol>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-6">
+				<a href="#menu-toggle" class="btn btn-default menu-toggle">Mostrar/Ocultar
+					código</a>
+				<h3>Elementos en página</h3>
+				<div>
+					<div class='row-fluid box-rounded'>
+						<ol class='nested_with_drop vertical' id='contenidoFormulario'>
+						</ol>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- /#page-content-wrapper -->
